1f04ea57b7d16129c935bc501ef38ed19a0b2d0c
Akimov <pursuit1@mail.ru>
2023-08-17 18:57:34 +0300

    First commit

---
+++ Homework9/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Homework9;
+using System.Diagnostics;
+
+
+List<int> lengthOfArray = new List<int> { 100_000, 1000_000, 10_000_000 };
+
+foreach (int length in lengthOfArray)
+{
+    var numbers = Enumerable.Range(0, length).
+        Select(_ => Random.Shared.Next(0, 100)).ToArray();
+    Console.WriteLine($"Array containing {length} elements has created");
+
+
+    {
+        var sw = Stopwatch.StartNew();
+        SumArray.Sum(numbers);
+        Console.WriteLine("Time is   " + sw.ElapsedMilliseconds);
+    }
+
+    {
+        var sw = Stopwatch.StartNew();
+        SumArray.SumParallel(numbers);
+        Console.WriteLine("Time is   " + sw.ElapsedMilliseconds);
+    }
+    {
+        var sw = Stopwatch.StartNew();
+        SumArray.SumPLINQ(numbers);
+        Console.WriteLine("Time is   " + sw.ElapsedMilliseconds);
+    }
+    Console.WriteLine(  );
+}
+
+
---
+++ Homework9/SumArray.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework9
+{
+    internal class SumArray
+    {
+
+        public static long Sum(int[] array) 
+        {
+           long sum = 0;
+            foreach (int item in array)
+            {
+                sum += item;
+            }
+            Console.WriteLine($"Method Sum {sum}");
+            return sum;
+        }
+
+      
+
+        public static long SumParallel(int[] array)
+        {
+            long sum = 0;
+            Parallel.For(0, array.Length, new ParallelOptions()
+            {
+                MaxDegreeOfParallelism = 4
+            }, i => {
+              
+                Interlocked.Add(ref sum, array[i]);
+
+            });
+            Console.WriteLine($"Method SumParallel {sum}");
+            return sum;
+        }
+        public static long SumPLINQ(int[] array)
+        {
+            long sum = array.AsParallel().Sum();
+            Console.WriteLine($"SumPLINQ {sum}");
+            return sum;
+        }
+        
+    }
+}
